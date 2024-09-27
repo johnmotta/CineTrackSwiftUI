e40325b1ed340c68525a35557436bcfb5ec0597c
johnmotta <gabriel_motta31@hotmail.com>
2024-09-27 09:29:49 -0300

    Create DB and logic CoreData

--- CineTrackSwiftUI/CoreData/Movie+extension.swift
+++ CineTrackSwiftUI/CoreData/Movie+extension.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import CoreData
+
+
+extension Movie {
+    
+    private static func request() -> NSFetchRequest<NSFetchRequestResult> {
+        let request: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: String(describing: Self.self))
+        request.returnsDistinctResults = true
+        request.returnsObjectsAsFaults = true
+        return request
+    }
+    
+    class func deleteAll(_ context: NSManagedObjectContext) {
+        let request = Movie.request()
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: request)
+        
+        do {
+            guard let persistentStoreCoordinator = context.persistentStoreCoordinator else { return }
+            try persistentStoreCoordinator.execute(deleteRequest, with: context)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
+    class func saveDatabase(_ context: NSManagedObjectContext) {
+        guard context.hasChanges else { return}
+        do {
+            try context.save()
+        } catch (let error) {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+}
+
--- CineTrackSwiftUI/CoreData/Persistence.swift
+++ CineTrackSwiftUI/CoreData/Persistence.swift
@@ -5,4 +5,46 @@
 //  Created by John on 26/09/24.
 //
 
-import Foundation
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+    let container: NSPersistentContainer
+    
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "Model")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+    
+    // Modo preview para SwiftUI
+    static var preview: PersistenceController = {
+        let result = PersistenceController(inMemory: true) // Banco de dados em memória
+        let viewContext = result.container.viewContext
+        
+        // Adiciona dados de exemplo para visualização no preview
+        for i in 0..<5 {
+            let movie = Movie(context: viewContext)
+            movie.originalTitle = "Title: \(i)"
+            movie.overview = "Overview: \(i)"
+            movie.releaseDate = "Release Date: \(i)"
+        }
+        
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+        
+        return result
+    }()
+
+}
