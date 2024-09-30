c6a8457601ea6b70b58042c15b0f757e041f5508
johnmotta <gabriel_motta31@hotmail.com>
2024-09-30 20:11:06 -0300

    Add ServiceManager methods for fetching movie details and cast

--- CineTrackSwiftUI/Service/ServiceManager.swift
+++ CineTrackSwiftUI/Service/ServiceManager.swift
@@ -43,4 +43,64 @@ class ServiceManager: MovieProtocol {
         }
         task.resume()
     }
+
+    func fetchMovieDetails(movieId: Int, completion: @escaping (Result<MovieDetail, NetworkError>) -> Void) {
+        let deviceLanguage = Locale.preferredLanguages.first ?? "en-US"
+        let urlString = "https://api.themoviedb.org/3/movie/\(movieId)?api_key=\(Constants.API)&language=\(deviceLanguage)"
+        
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.invalidURL(urlString)))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(.networkFailure(error)))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.noData))
+                return
+            }
+            
+            do {
+                let movieDetails = try JSONDecoder().decode(MovieDetail.self, from: data)
+                completion(.success(movieDetails))
+            } catch {
+                completion(.failure(.decodingError(error)))
+            }
+        }.resume()
+    }
+
+    
+    func fetchMovieCast(movieId: Int, completion: @escaping (Result<[Cast], NetworkError>) -> Void) {
+        let deviceLanguage = Locale.preferredLanguages.first ?? "en-US"
+        let urlString = "https://api.themoviedb.org/3/movie/\(movieId)/credits?api_key=\(Constants.API)&language=\(deviceLanguage)"
+        
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.invalidURL(urlString)))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(.networkFailure(error)))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.noData))
+                return
+            }
+            
+            do {
+                let credits = try JSONDecoder().decode(Credits.self, from: data)
+                completion(.success(credits.cast))
+            } catch {
+                completion(.failure(.decodingError(error)))
+            }
+        }.resume()
+    }
+
 }
--- CineTrackSwiftUI/View/MovieDetailView.swift
+++ CineTrackSwiftUI/View/MovieDetailView.swift
@@ -5,4 +5,121 @@
 //  Created by John on 30/09/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct MovieDetailView: View {
+    @State private var movieDetail: MovieDetail?
+    @State private var movieCast: [Cast] = []
+    
+    let movieId: Int
+    
+    var body: some View {
+        VStack {
+            if let movieDetail = movieDetail {
+                AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w500\(movieDetail.backdropPath ?? "")"))
+                    .scaledToFit()
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text(movieDetail.title)
+                            .font(.title2)
+                            .bold()
+                        
+                        Text(movieDetail.releaseDate.prefix(4))
+                            .padding(.leading, 5)
+                    }
+                    .padding(.horizontal)
+                    
+                    HStack {
+                        Text("\(movieDetail.runtime ?? 0)m")
+                        Text(" | ")
+                        Text(movieDetail.genres.map { $0.name }.joined(separator: ", "))
+                    }
+                    .padding(.horizontal)
+                    .lineLimit(2)
+                    ScrollView {
+
+                    Divider()
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 16) {
+                            ForEach(movieCast.prefix(10), id: \.name) { cast in
+                                VStack {
+                                    if let profilePath = cast.profilePath {
+                                        AsyncImage(
+                                            url: URL(string: "https://image.tmdb.org/t/p/w185\(profilePath)")
+                                        ) { image in
+                                            image
+                                                .resizable()
+                                                .scaledToFill()
+                                                .frame(width: 100, height: 150)
+                                                .cornerRadius(8)
+                                        } placeholder: {
+                                            Color.gray
+                                                .frame(width: 100, height: 150)
+                                                .cornerRadius(8)
+                                        }
+                                    } else {
+                                        // Caso o ator não tenha imagem
+                                        Color.gray
+                                            .frame(width: 100, height: 150)
+                                            .cornerRadius(8)
+                                    }
+                                    
+                                    Text(cast.name)
+                                        .font(.caption)
+                                        .frame(width: 100)
+                                        .multilineTextAlignment(.center)
+                                    
+                                    Text(cast.character)
+                                        .font(.caption2)
+                                        .foregroundColor(.gray)
+                                        .frame(width: 100)
+                                        .multilineTextAlignment(.center)
+                                }
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+
+                    Divider()
+                    
+                        Text(movieDetail.overview)
+                            .padding(.bottom, 20)
+                            .padding(.horizontal)
+                    }
+                }
+            } else {
+                Text("Carregando detalhes do filme...")
+                
+            }
+        }
+        .onAppear {
+            ServiceManager.shared.fetchMovieDetails(movieId: movieId) { result in
+                switch result {
+                case .success(let details):
+                    DispatchQueue.main.async {
+                        self.movieDetail = details
+                    }
+                case .failure(let error):
+                    print("Erro ao carregar detalhes do filme: \(error)")
+                }
+            }
+            
+            ServiceManager.shared.fetchMovieCast(movieId: movieId) { result in
+                switch result {
+                case .success(let cast):
+                    DispatchQueue.main.async {
+                        self.movieCast = cast
+                    }
+                case .failure(let error):
+                    print("Erro ao carregar elenco: \(error)")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    MovieDetailView(movieId: 0)
+}
