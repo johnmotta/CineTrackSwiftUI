76c8a1c3f90411b6e30883da6e43a2eb67b82b4d
johnmotta <gabriel_motta31@hotmail.com>
2024-09-30 08:49:30 -0300

    Implement toolbar item view with Sections enum

--- CineTrackSwiftUI/View/ToolBarItemView.swift
+++ CineTrackSwiftUI/View/ToolBarItemView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct ToolBarItemView: View {
+    @Binding var segment: Sections
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 0) {
+            ForEach(Sections.allCases, id: \.self) { buttonTitle in
+                let buttonTitle = buttonTitle
+                Button(action: {
+                    withAnimation(.easeInOut(duration: 0.3)) {
+                        segment = buttonTitle
+                    }
+                }) {
+                    Text(buttonTitle.description)
+                        .padding(.vertical, 8)
+                        .padding(.horizontal, 12)
+                        .background(
+                            Capsule()
+                                .fill(segment == buttonTitle ? Color.primary : Color.clear)
+                        )
+                        .foregroundColor(segment == buttonTitle ? .purple : .primary)
+                }
+            }
+        }
+        .background(Color.gray.opacity(0.2))
+        .clipShape(Capsule())
     }
 }
 
 #Preview {
-    ToolBarItemView()
+    ToolBarItemView(segment: .constant(.upcoming))
 }
